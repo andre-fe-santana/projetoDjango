edabad7900826b68dafd8898feb8e7fa38fea7eb
André <castroferreiraandre@gmail.com>
2025-03-24 16:48:15 -0300

    v0.4 - Criando modelos

--- sistema/admin.py
+++ sistema/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from sistema import models
 
-# Register your models here.
+# Aqui fica o registro do Usuário (classe)
+@admin.register(models.Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'email', 'ativo',)
